9d7cee1e1df1fbe805d3ff797999dbf90848985d
Jing Zhang <jing.zhang@oliverwyman.com>
2024-04-01 18:07:07 +0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
+import sitemap from '@astrojs/sitemap';
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://j-astro-1.netlify.app",
+  integrations: [sitemap()],
   integrations: [preact()],
   prefetch: true,
   output: "server",
