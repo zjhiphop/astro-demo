88859cb49565b645579ed5da24f655d6e958568e
Jing Zhang <jing.zhang@oliverwyman.com>
2024-04-01 16:22:32 +0800

    add preact islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site:"https://j-astro-1.netlify.app"
+  site: "https://j-astro-1.netlify.app",
+  integrations: [preact()]
 });
