e331a99384a7b487de7ccd7b419a3e8984f981b2
Jing Zhang <jing.zhang@oliverwyman.com>
2024-04-01 18:04:34 +0800

    netlify ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://j-astro-1.netlify.app",
-  integrations: [preact()]
+  integrations: [preact()],
+  prefetch: true,
+  output: "server",
+  adapter: netlify({
+    edgeMiddleware: true,
+    cacheOnDemandPages: true,
+  })
 });
