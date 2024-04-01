5a5fdbe3ad64e54cfb3576310656de0d98b5b36e
Jing Zhang <jing.zhang@oliverwyman.com>
2024-04-01 18:10:17 +0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,25 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: "https://j-astro-1.netlify.app",
-  integrations: [sitemap()],
+  integrations: [sitemap({
+    // Filter out pages that should not be included in the sitemap
+    // filter: (page) => page !== 'https://stargazers.club/secret-vip-lounge/',
+
+    // Add custom pages to the sitemap
+    // customPages: ['https://stargazers.club/external-page', 'https://stargazers.club/external-page2'],
+    entryLimit: 10000,
+    changefreq: 'weekly',
+    priority: 0.7,
+
+    i18n: {
+        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+          es: 'es-ES',
+          fr: 'fr-CA',
+        },
+      },
+  })],
   integrations: [preact()],
   prefetch: true,
   output: "server",
