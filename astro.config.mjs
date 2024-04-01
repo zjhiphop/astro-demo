a307184cf0a51b0b8d49e5a4af7fb6d915dbffc5
Jing Zhang <jing.zhang@oliverwyman.com>
2024-04-01 18:31:18 +0800

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,36 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
 import netlify from "@astrojs/netlify";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://j-astro-1.netlify.app",
-  integrations: [sitemap({
-    // Filter out pages that should not be included in the sitemap
-    // filter: (page) => page !== 'https://stargazers.club/secret-vip-lounge/',
+  integrations: [
+    tailwind({
+        applyBaseStyles: true,
+    }),
+    preact(),
+    sitemap({
+        // Filter out pages that should not be included in the sitemap
+        // filter: (page) => page !== 'https://stargazers.club/secret-vip-lounge/',
 
-    // Add custom pages to the sitemap
-    // customPages: ['https://stargazers.club/external-page', 'https://stargazers.club/external-page2'],
-    entryLimit: 10000,
-    changefreq: 'weekly',
-    priority: 0.7,
+        // Add custom pages to the sitemap
+        // customPages: ['https://stargazers.club/external-page', 'https://stargazers.club/external-page2'],
+        entryLimit: 10000,
+        changefreq: 'weekly',
+        priority: 0.7,
 
-    i18n: {
-        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
-        locales: {
-          en: 'en-US', // The `defaultLocale` value must present in `locales` keys
-          es: 'es-ES',
-          fr: 'fr-CA',
+        i18n: {
+            defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+            locales: {
+            en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+            es: 'es-ES',
+            fr: 'fr-CA',
+            },
         },
-      },
-  })],
-  integrations: [preact()],
+    })
+],
   prefetch: true,
   output: "server",
   adapter: netlify({
