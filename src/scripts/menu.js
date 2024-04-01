a5c898426cc35c3f8440a3be4e40bb2797ca80ca
Jing Zhang <jing.zhang@oliverwyman.com>
2024-04-01 17:25:02 +0800

    page transition

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,7 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
+document.addEventListener('astro:page-load', () => {
+
+  document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
   });
+
+})
